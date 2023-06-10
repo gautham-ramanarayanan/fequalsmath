6a915e89c1012fada3665002ab7c959f9fd63432
exinr <gauthamr7@outlook.com>
2023-06-10 00:19:39 -0500

    Changed NavBar color

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-    <header className="bg-gray-800 text-white sticky top z-10">
+    <header className="bg-blue-500 text-white sticky top z-10">
             <section className="max-w-4xl mx-auto p-4 flex justify-between items-center">
                 <h1 className="text-3xl font-medium">
                     <a href="#main">F=Math</a>
@@ -11,16 +11,21 @@ export default function Home() {
                 <button className="md:hidden text-3xl">&#9776;</button>
                 <nav className="hidden md:inline space-x-8">
                     <a href="#Instructors" className="hover:opacity-80">Instructors</a>
-                    <a href="#Curriculum" className="hover:opacity-90">Curriculum</a>
+                    <a href="#Curriculum" className="hover:opacity-80">Curriculum</a>
                     <a href="#Testimonials" className="hover:opacity-80">Testimonials</a>
-                    <a href="#Past Events" className="hover:opacity-90">Past Events</a>
-                    <a href="#Upcoming" className="hover:opacity-90">Upcoming</a>
-                    <a href="#FAQ" className="hover:opacity-90">FAQ</a>
+                    <a href="#Past Events" className="hover:opacity-80">Past Events</a>
+                    <a href="#Upcoming" className="hover:opacity-80">Upcoming</a>
+                    <a href="#FAQ" className="hover:opacity-80">FAQ</a>
                 </nav>
                 
             </section>
         </header>
-    <main className="min-h-screen bg-slate-50 dark:bg-black dark:text-white">
+    <main className="min-h-screen bg-slate-50" id="main">
+        <section className="px-40 py-4 mx-auto flex justify-center">
+            <div className="relative lg:p-40 lg:my-0.5">
+                <Image src="/groupphoto.png" alt="Group Photo from F=Math" fill= "true" style={{objectFit: "contain"}} />
+            </div>
+        </section>
         
     </main>
 
