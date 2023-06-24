afa2b8f2fbe3179c9267100b7cf6ca9145768277
exinr <gauthamr7@outlook.com>
2023-06-24 00:24:10 -0500

    Changed site description

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'F=Math',
-  description: 'The taste of math your child needs',
+  description: 'Non-profit math competition prep.',
 }
 
 export default function RootLayout({ children }) {
